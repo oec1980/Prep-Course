b71d09589c7bab959eb67584021bc116c80a8ec6
unknown <mylantechnology@gmail.com>
2022-07-07 14:42:00 -0500

    test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -200,7 +200,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola ' + nombre + '!';
+  return `hola ${nombre}!`;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
