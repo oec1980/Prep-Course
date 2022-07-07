19723caabc045efe040f6eaff237886980b26c4b
unknown <mylantechnology@gmail.com>
2022-07-07 08:23:52 -0500

    Resolviendo el homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola';
+const nuevaString = 'Nini Esta Loquita';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum =  8;
@@ -34,7 +34,7 @@ function devolverString(str) {
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
+  // Tu código: x +y 
   
 }
 
