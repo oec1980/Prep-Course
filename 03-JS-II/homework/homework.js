d78705b1e990454243d372b1b02a960594ad74a8
unknown <mylantechnology@gmail.com>
2022-07-09 16:55:40 -0500

    Test faltantes

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -96,10 +96,7 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
- if (numero < 50  && numero > 20 ){
-  return 'true';
- }
-  return 'false';
+   return numero < 50 && numero >20;
 }
 
 function esEntero(numero) {
@@ -110,10 +107,8 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if (numero % 1 === 0){
-    return 'true';
-  }
-  return 'false';
+  return numero % 1 === 0 ;
+
 }
 
 function fizzBuzz(numero) {
@@ -140,16 +135,40 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 < 0 || num2 < 0 || num3 < 0){
+    return 'Hay negativos';
+  }
+  else if (num1 > 0 && num1 > num2  && num1 > num3 ){
+   return 'Número 1 es mayor y positivo';
+}
+else if (num1 === 0 || num2 === 0  || num3 === 0){
+  return 'Error';
+}
+else if (num3 > num1 && num3 > num2 ){
+  return num3 +1;
+}
+  else {
+    return false ;
+  }
 
 }
-
-
-function esPrimo(numero) {
+function esPrimo(num) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+if (num < 2 ){
+  return false;
+}
+ if (num % 2){
+  return true;
+}
+for (var i =2; i< num; i++){
+  if (num % 1 === 0){
+    return false;
+  }
+}
 }
 
 function esVerdadero(valor){
