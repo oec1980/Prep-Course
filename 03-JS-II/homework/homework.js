2ba59782d04261c060e02b47a9edd422593e8041
unknown <mylantechnology@gmail.com>
2022-07-11 15:08:35 -0500

    test Finalizado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -152,49 +152,72 @@ else if (num3 > num1 && num3 > num2 ){
   }
 
 }
-function esPrimo(num) {
+function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-if (num < 2 ){
-  return false;
-}
- if (num % 2){
-  return true;
-}
-for (var i =2; i< num; i++){
-  if (num % 1 === 0){
+  
+  if( numero < 2) {
     return false;
   }
+  if(numero === 2) {
+    return true;
+  }
+  for( i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
-}
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if(valor === true){
+    return 'Soy verdadero';
+  } 
+  else if (valor === false){
+  return 'Soy falso';
 }
-
+}
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   
+  var arrayTablaDel6 = []
+  for (var i = 0 ; i < 11 ; i++){
+    arrayTablaDel6.push(i*6)
+  }
+  return arrayTablaDel6;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if ( numero > 99 && numero < 999){
+    return true;
+  }
+  return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+  var x = numero;
+   do {
+    i = i +1;
+    x = x + 5;
+   }
+   while (i < 8);
+  return x;
 }
 
 
